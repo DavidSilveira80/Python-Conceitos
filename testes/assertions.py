098d0d4573e4c45cd471f7935c3af33f48410828
DavidSilveira80 <davidpscontato@gmail.com>
2023-12-03 21:59:40 -0300

    Testes, PQ testar, Assert.

---
+++ testes/assertions.py
@@ -0,0 +1,43 @@
+"""
+Assertions (Afirmações/checagens/questionamentos)
+
+Em Python utilizamos a palavra 'assert' para realizar simples afirmações utilizadas nos testes.
+
+utilizamos o 'assert' em uma expressão que queremos checar se é válida ou não.
+Se a expressão for verdadeira, retorna None e caso seja falsa levanta um erro do tipo AssertionError.
+
+# OBS: Nós podemos especificar, opcionalmente, um segundo argumento ou mesmo uma mensagem de erro personalizada
+
+# OBS: A palavra 'assert' pode ser utilizada em qualquer função ou código nosso... naõ precisa ser exclusivamente
+nos testes.
+
+# ALERTA: Cuidado ao utilizar 'assert'
+
+Se um programa Python for executado com o parâmetro -O, nenhum assertion será validado. Ou seja, todas as
+validações já eram.
+"""
+assert 4 == 4
+# assert 4 == 2, '4 não é igual a 2'
+
+def soma_numeros_positivos(a, b):
+    assert a > 0 and b > 0 , 'Ambos precisam ser positivos'
+    return a + b
+
+ret = soma_numeros_positivos(2, 4) # 6
+print(ret)
+
+# ret = soma_numeros_positivos(-4, 5) # AssertionError
+
+def comer_fast_food(comida):
+    assert comida in [
+        'pizza',
+        'sorvete',
+        'doces',
+        'batata frita',
+        'cachorro quente',
+    ], 'A comida precisa ser fast food'
+    return f'Eu estou comendo {comida}'
+
+print(comer_fast_food('sorvete'))
+#print(comer_fast_food('sopa'))
+
---
+++ testes/por_que_testar_codigo.py
@@ -0,0 +1,32 @@
+"""
+Por que testar nosso Código?
+
+Testes Automátizados
+
+------------------------------------
+/                                  /
+/       FrontEnd e BackEnd         /
+/                                  /
+------------------------------------
+/       testes automátizados       /
+-----------------------------------/
+
+Por que testar nosso Código?
+    - Reduzir bugs (problemas) no código existente;
+    - Testes garantem que novos recursos da sua aplicação não quebrem (alterem) recursos antigos;
+    - Testes garantem que bugs (problemas) que foram corrigidos anteriormente continuam corrigidos;
+    - Testes garantem que a refatoração que costumamos fazer não tragam novos bugs (problemas);
+
+TDD - Test Driven Development (Desenvolvimento Guiado por Testes)
+
+Com TDD é utlizado estágios de desenvolvimento:
+    - Você escreve seu teste primeiro;
+    - Então você escreve o código mínimo suficiente para fazer o teste passar (ou seja, executar com sucesso);
+    - Então refatora o código para realizar a funcionalidade e testa novamente;
+    - Uma vez que o teste passe, o recurso é considerado completo;
+
+Este estágios de desenvolvimento do TDD são quase como um mantra que os desenvolvedores seguem, conhecidos como:
+     - RED;
+     - GREEN;
+     - REFACTOR;
+"""
