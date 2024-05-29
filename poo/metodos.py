a1fd38620dba622d540415dbf071b6f48f1b2b22
DavidSilveira80 <davidpscontato@gmail.com>
2024-05-29 14:58:36 -0300

    Revisão de métodos.

--- poo/metodos.py
+++ poo/metodos.py
@@ -11,13 +11,13 @@ Em Python, dividimos os métodos, em 2 grupos: Métodos de instância e Métodos
 # O método dunder init __init__ é um método especial chamado de construtor e sua função é construir o objeto
 a partir da classe.
 
-OBS: Os elmentos em Python que inicia e finaliza com duplo underline __ é chamado de dunder (Double Underline)
+OBS: Todo elemento em Python que inicia e finaliza com duplo underline __ é chamado de dunder (Double Underline)
 
 OBS: Os métodos/funções dunder em Python são chamados de métodos mágicos.
 
-ATENÇÃO!! Por mias que possamos criar nossas próprias funções utilizando dunder não é aconselhado. Python
+ATENÇÃO!! Por mais que possamos criar nossas próprias funções utilizando dunder não é aconselhado. Python
 possui vários métodos com esta nomenclatura e pode ser que mudemos o comportamento dessas funções mágicas
-internas da linguagem. Então, evite ao máximo. De preferência nnca os faça.
+internas da linguagem. Então, evite ao máximo. De preferência nunca os faça.
 
 # Métodos são escritos em letras minúsculas. Se o nome for composto, o nome terá as palavras separadas por
 underline _ (snack_case).
@@ -25,6 +25,41 @@ underline _ (snack_case).
 # Métodos de classe em Python são conhecidos como métodos Estáticos em outras linguagens.
 
 """
+
+
+class Lampada:
+    def __init__(self, cor, voltagem, luminosidade):
+        self.cor = cor
+        self.voltagem = voltagem
+        self.luminosidade = luminosidade
+        self.ligada = False
+
+
+class ContaCorrente:
+
+    contador = 4999
+
+    def __init__(self, limite, saldo):
+        self.numero = ContaCorrente.contador + 1
+        self.limite = limite
+        self.saldo = saldo
+        ContaCorrente.contador = self.numero
+
+
+class Produto:
+
+    contador = 0
+
+    def __init__(self, nome, descricao, valor):
+        self.id = Produto.contador + 1
+        self.nome = nome
+        self.descricao = descricao
+        self.valor = valor
+        Produto.contador = self.id
+
+    def desconto(self, porcentagem):  # Método de Instância
+        """Retorna o valor do produto com o desconto"""
+        return (self.valor * (100 - porcentagem)) / 100
 
 
 class Usuario:
